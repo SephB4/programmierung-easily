0f35e3d85e752f9532adeb51fd5ea46b835d00ee
SephB4 <168664961+SephB4@users.noreply.github.com>
2024-05-06 20:15:52 +0200

    Add files via upload

---
+++ R39_Tabellen_2.R
@@ -0,0 +1,13 @@
+codeoceanR::rt_score()
+
+# A4 ----
+# Lese die Geburten-Daten in R ein. Beachte, dass NAs als 999 eingetragen sind.
+geburten <- read.table("Geburten-Daten.txt", na.strings = "999", header = TRUE)
+
+# A5 ----
+# Schreibe den eingebauten Datensatz 'mtcars' in eine Textdatei names motor_trend.txt.
+# Unterdrücke die Einbeziehung von rownames.
+# Die Spaltennamen sollten nicht in Anführungszeichen gesetzt werden.
+
+
+# Mache weiter in "R39_Tabellen_3.R"
